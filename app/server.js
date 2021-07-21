d0a333758d2de6459cc10bdb23bc78e5c7375a78
Demilade Oyewusi <tayodemi@gmail.com>
2021-07-21 23:22:00 +0100

    module 7: networks - container to container

--- app/server.js
+++ app/server.js
@@ -1,19 +1,56 @@
-const fastify = require('fastify')({ logger: true })
-fastify.register(require('fastify-routes'))
-fastify.register(require('fastify-axios'))
+const fastify = require("fastify")({ logger: true });
+fastify.register(require("fastify-routes"));
+const mongoose = require("mongoose");
+
+let ProfileSchema = new mongoose.Schema({
+ name: String,
+ age: Number,
+ popular: String,
+});
+
+Profile = mongoose.model("Profile", ProfileSchema);
+
+const mongoUrl = "mongodb://mongodb:27017/profiles";
+
+try {
+ mongoose.connect(mongoUrl);
+} catch (error) {
+ console.error(error);
+}
 
 //application routes
-fastify.get('/books', async (request, reply) => {
-   const { data, status } = await fastify.axios.get("http://127.0.0.1:8300/ping"); // change endpoint to host.docker.internal:8300/ping
-       reply.send(data)
-})
+fastify.get("/profiles", (request, reply) => {
+ Profile.find({}, (err, profiles) => {
+   if (!err) {
+     reply.send(profiles);
+   } else {
+     reply.send({ error: err });
+   }
+ });
+});
 
+fastify.post("/profile/add", (request, reply) => {
+ const profile = request.body;
+ console.log("the profile", profile);
+
+ Profile.create(profile, (err, profile) => {
+   if (!err) {
+     reply.send(profile);
+   } else {
+     reply.send({ error: err });
+   }
+ });
+
+ reply.send(profile);
+});
 
 //server
-fastify.listen(80,'0.0.0.0', (error) => {
-   if (error) {
-       console.log(error)
-   } else {
-       console.log(`Server running, navigate to  http://localhost:80`)
-   }
-})
+fastify.listen(80, "0.0.0.0", (error) => {
+ if (error) {
+   console.log(error);
+ } else {
+   console.log(`Server running, navigate to  http://localhost:80`);
+ }
+});
+
+
